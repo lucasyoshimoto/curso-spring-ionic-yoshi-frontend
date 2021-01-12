a68026a03976153c28bb67e33986340e2a79938c
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-12 12:11:42 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -32,9 +32,7 @@ export class CategoriasPage {
       (response) => {
         this.items = response;
       },
-      (error) => {
-        console.log(error);
-      }
+      (error) => {}
     );
   }
 }
