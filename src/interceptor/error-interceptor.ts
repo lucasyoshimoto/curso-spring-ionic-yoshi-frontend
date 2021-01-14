4fb10af88fd6a9eaca559af7c546a0f361e6b723
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-14 16:40:13 -0300

    Tratamento padrao para outros erros com Alert

--- src/interceptor/error-interceptor.ts
+++ src/interceptor/error-interceptor.ts
@@ -8,16 +8,19 @@ import {
 } from "@angular/common/http";
 import { Observable } from "rxjs/Rx"; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from "../services/storage.service";
+import { AlertController } from "ionic-angular/components/alert/alert-controller";
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(public storage: StorageService) {}
+  constructor(
+    public storage: StorageService,
+    public alertCtrl: AlertController
+  ) {}
 
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    // console.log("Passou no interceptor"); - Teste Interceptor
     return next.handle(req).catch((error, caught) => {
       let errorObj = error;
       if (errorObj.error) {
@@ -26,22 +29,55 @@ export class ErrorInterceptor implements HttpInterceptor {
       if (!errorObj.status) {
         errorObj = JSON.parse(errorObj);
       }
-
       console.log("Erro detectado pelo interceptor:");
       console.log(errorObj);
 
       switch (errorObj.status) {
+        case 401:
+          this.handle401();
+          break;
+
         case 403:
           this.handle403();
           break;
+
+        default:
+          this.handleDefaultEror(errorObj);
       }
 
       return Observable.throw(errorObj);
     }) as any;
   }
-
   handle403() {
     this.storage.setLocalUser(null);
+  }
+
+  handle401() {
+    let alert = this.alertCtrl.create({
+      title: "Erro 401: falha de autenticação",
+      message: "Email ou senha incorretos",
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: "Ok",
+        },
+      ],
+    });
+    alert.present();
+  }
+
+  handleDefaultEror(errorObj) {
+    let alert = this.alertCtrl.create({
+      title: "Erro " + errorObj.status + ": " + errorObj.error,
+      message: errorObj.message,
+      enableBackdropDismiss: false,
+      buttons: [
+        {
+          text: "Ok",
+        },
+      ],
+    });
+    alert.present();
   }
 }
 
