73c86c8c62f7f36f48dc11146949c83566b8d9bd
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-01-14 17:00:46 -0300

    Fazendo o app aproveitar o usuario logado na tela inicial

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,9 +1,8 @@
 import { Component } from "@angular/core";
-import { IonicPage, NavController } from "ionic-angular";
+import { NavController, IonicPage } from "ionic-angular";
 import { MenuController } from "ionic-angular/components/app/menu-controller";
 import { CredenciaisDTO } from "../../models/credenciais.dto";
 import { AuthService } from "../../services/auth.service";
-
 @IonicPage()
 @Component({
   selector: "page-home",
@@ -14,18 +13,27 @@ export class HomePage {
     email: "",
     senha: "",
   };
-
   constructor(
     public navCtrl: NavController,
     public menu: MenuController,
     public auth: AuthService
   ) {}
-
   ionViewWillEnter() {
     this.menu.swipeEnable(false);
   }
+
   ionViewDidLeave() {
     this.menu.swipeEnable(true);
+  }
+
+  ionViewDidEnter() {
+    this.auth.refreshToken().subscribe(
+      (response) => {
+        this.auth.successfulLogin(response.headers.get("Authorization"));
+        this.navCtrl.setRoot("CategoriasPage");
+      },
+      (error) => {}
+    );
   }
 
   login() {
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,22 +1,30 @@
 import { Injectable } from "@angular/core";
 import { CredenciaisDTO } from "../models/credenciais.dto";
 import { HttpClient } from "@angular/common/http";
-import { API_CONFIG } from "../config/api.cofig";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from "./storage.service";
 import { JwtHelper } from "angular2-jwt";
-
+import { API_CONFIG } from "../config/api.cofig";
 @Injectable()
 export class AuthService {
   jwtHelper: JwtHelper = new JwtHelper();
-
   constructor(public http: HttpClient, public storage: StorageService) {}
-
   authenticate(creds: CredenciaisDTO) {
     return this.http.post(`${API_CONFIG.baseUrl}/login`, creds, {
       observe: "response",
       responseType: "text",
     });
+  }
+
+  refreshToken() {
+    return this.http.post(
+      `${API_CONFIG.baseUrl}/auth/refresh_token`,
+      {},
+      {
+        observe: "response",
+        responseType: "text",
+      }
+    );
   }
 
   successfulLogin(authorizationValue: string) {
