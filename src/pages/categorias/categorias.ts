2df06d4ddf286ee30609933b48ce5133412aecb4
Lucas Yoshimoto <lucasyoshimoto238@gmail.com>
2021-02-25 15:34:42 -0300

    Criando pagina de produtos

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,38 +1,36 @@
-import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
-import { API_CONFIG } from "../../config/api.config";
-import { CategoriaDto } from "../../models/categoria.dto";
-import { CategoriaService } from "../../services/domain/categoria.service";
-
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 /**
  * Generated class for the CategoriasPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
 @IonicPage()
 @Component({
-  selector: "page-categorias",
-  templateUrl: "categorias.html",
+  selector: 'page-categorias',
+  templateUrl: 'categorias.html',
 })
 export class CategoriasPage {
   bucketUrl: string = API_CONFIG.bucketBaseUrl;
-
-  items: CategoriaDto[];
-
+  items: CategoriaDTO[];
   constructor(
-    public navCtrl: NavController,
+    public navCtrl: NavController, 
     public navParams: NavParams,
-    public categoriaService: CategoriaService
-  ) {}
-
+    public categoriaService: CategoriaService) {
+  }
   ionViewDidLoad() {
-    this.categoriaService.findAll().subscribe(
-      (response) => {
+    this.categoriaService.findAll()
+      .subscribe(response => {
         this.items = response;
       },
-      (error) => {}
-    );
+      error => {});
+  }
+
+  showProdutos() {
+    this.navCtrl.push('ProdutosPage');    
   }
 }
